8487acff1c96cb4a38c99405e8b0bf5b5944a92c
ARTSHEEP\gal <gala.sheep@outlook.com>
2026-01-30 18:02:38 +0200

    game play menu screen

---
+++ Assets/Scripts/GameMenuUI.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameMenuUI : MonoBehaviour
+{
+    [Header("Menu UI")]
+    [SerializeField] private GameObject menuRoot;
+    [SerializeField] private Button playButton;
+
+    [Header("Start behavior")]
+    [SerializeField] private bool activateOnStart = true;
+    [SerializeField] private bool fadeOnStart = true;
+
+    [Header("Play behavior")]
+    [SerializeField] private bool fadeOnPlay = true;
+    [SerializeField] private UnityEvent onPlay;
+
+    private GameSettings gameSettings;
+    private bool buttonHooked;
+
+    private void Awake()
+    {
+        if (menuRoot == null)
+        {
+            menuRoot = gameObject;
+        }
+
+        gameSettings = FindFirstObjectByType<GameSettings>();
+        HookPlayButton();
+
+        if (activateOnStart && fadeOnStart)
+        {
+            menuRoot.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        if (!activateOnStart)
+        {
+            return;
+        }
+
+        if (fadeOnStart && gameSettings != null)
+        {
+            menuRoot.SetActive(false);
+            gameSettings.FadeAndSwitch(() => menuRoot.SetActive(true));
+            return;
+        }
+
+        menuRoot.SetActive(true);
+    }
+
+    private void HookPlayButton()
+    {
+        if (buttonHooked || playButton == null)
+        {
+            return;
+        }
+
+        playButton.onClick.AddListener(HandlePlayClicked);
+        buttonHooked = true;
+    }
+
+    private void HandlePlayClicked()
+    {
+        if (fadeOnPlay && gameSettings != null)
+        {
+            gameSettings.FadeAndSwitch(() =>
+            {
+                menuRoot.SetActive(false);
+                onPlay?.Invoke();
+            });
+            return;
+        }
+
+        menuRoot.SetActive(false);
+        onPlay?.Invoke();
+    }
+}
